8abe86065cebf61ad8e39997e99019e0ac046849
Edevaldo Mazzucco <edevaldomazzucco1999@gmail.com>
2017-06-25 21:57:42 -0300

    Adicionado metodo de Listagem do Usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -56,6 +56,39 @@ class Usuario {
 		}
 	}
 
+	//Busca todos os usuarios
+	public static function getList(){
+		$sql = new SQL();
+		$result = $sql->select("SELECT * FROM tb_usuarios order by login");
+		return $result;
+	}
+
+	//busca somente os usuarios através de pesquisa
+	public static function search($login){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE login LIKE :SEARCH ORDER BY login", array(
+				':SEARCH'=>"%".$login."%"
+			));
+	}
+
+	//Faz login e carrega um usuario
+	public function login($login, $password){
+		$sql = new SQL();
+		$total = $sql->select("SELECT * FROM tb_usuarios WHERE login = :LOGIN AND senha = :SENHA", array(
+			':LOGIN'=>$login,
+			':SENHA'=>$password
+			));
+		if(count($total) > 0){
+			$row = $total[0];
+			$this->setId($row['id']);
+			$this->setLogin($row['login']);
+			$this->setSenha($row['senha']);
+			$this->setCadastro(new DateTime($row['cadastro']));
+		}else{
+			throw new Exception("Login e/ou senha inválidos.");
+		};
+	}
+
 	public function __toString(){
 		return json_encode(array(
 			"id"=>$this->getId(),
--- index.php
+++ index.php
@@ -2,9 +2,22 @@
 
 require_once("config.php");
 
-$user = new Usuario();
-$user->loadById(2);
+//Carrega um usuario
+//$user = new Usuario();
+//$user->loadById(2);
+//echo $user;
 
-echo $user;
+//Carrega um lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+//$busca = Usuario::search("u");
+//echo json_encode($busca);
+
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("user", "pass");
+echo $usuario;
 
 ?>
